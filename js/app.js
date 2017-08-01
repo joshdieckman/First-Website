097b3772eb17ddb0db17aa63ba9f7ea919efa449
Josh Dieckman <josh.dieckman@gmail.com>
2017-07-31 23:34:07 -0400

    fixed the links and linked the education page to the external javascript file

--- js/app.js
+++ js/app.js
@@ -6,5 +6,24 @@ $(document).ready(function(){
     });
 });
 
+/*--------- Education page JavaScript --------------*/
+
+$(document).ready(function(){
+    $(".doss").click(function (){
+    $(".dosspopup").toggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".iwu").click(function (){
+    $(".iwupopup").toggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $(".codelouisville").click(function (){
+    $(".codelouisvillepopup").toggle("slow");
+    });
+});
 
 
