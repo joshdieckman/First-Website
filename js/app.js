03b6af51d268237d785cdfc2eea163d9bd2e135c
Josh Dieckman <josh.dieckman@gmail.com>
2017-07-31 23:18:27 -0400

    Linked to external javascript file for the experience page and fixed the links

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/*--------- Experience Page JavaScript -------------*/
+
+$(document).ready(function(){
+    $(".ann-inc").click(function (){
+    $(".ann-incpopup").toggle("slow");
+    });
+});
+
+
+
